cc763e9c6270f58be420c90faa7e2715747a1083
Matthew Purland <m.purland@gmail.com>
2015-12-23 13:06:10 -0800

    Preparable

---
+++ Morpheus/Preparable.swift
@@ -0,0 +1,91 @@
+import UIKit
+import ReactiveCocoa
+
+protocol Preparable {
+    func prepare()
+}
+
+extension Preparable where Self: UICollectionReusableView {
+    var rac_prepareForReuse: SignalProducer<Void, NoError> {
+        return rac_producerForSelector("prepareForReuse")
+    }
+}
+
+class PreparableTableCell: UITableViewCell {
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        commonInit()
+    }
+    
+    private func commonInit() {
+        var onceToken: dispatch_once_t = 0
+        dispatch_once(&onceToken) {
+            self.prepare()
+        }
+    }
+}
+
+extension PreparableTableCell: Preparable {
+    func prepare() {}
+}
+
+class PreparableCollectionCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        commonInit()
+    }
+    
+    private func commonInit() {
+        var onceToken: dispatch_once_t = 0
+        dispatch_once(&onceToken) {
+            self.prepare()
+        }
+    }
+}
+
+extension PreparableCollectionCell: Preparable {
+    func prepare() {}
+}
+
+class PreparableCollectionReusableView: UICollectionReusableView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        commonInit()
+    }
+    
+    private func commonInit() {
+        var onceToken: dispatch_once_t = 0
+        dispatch_once(&onceToken) {
+            self.prepare()
+        }
+    }
+}
+
+extension PreparableCollectionReusableView: Preparable {
+    func prepare() {}
+}
