33f66da271671381ce705a853f716098405be997
Matthew Purland <m.purland@gmail.com>
2016-01-05 16:13:27 -0800

    Added PreparableView

--- Morpheus/Preparable.swift
+++ Morpheus/Preparable.swift
@@ -5,6 +5,33 @@ protocol Preparable {
     func prepare()
 }
 
+class PreparableView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        commonInit()
+    }
+    
+    private func commonInit() {
+        var onceToken: dispatch_once_t = 0
+        dispatch_once(&onceToken) {
+            self.prepare()
+        }
+    }
+}
+
+extension PreparableView: Preparable {
+    func prepare() {}
+}
+
 extension Preparable where Self: UICollectionReusableView {
     var rac_prepareForReuse: SignalProducer<Void, NoError> {
         return rac_producerForSelector("prepareForReuse")
